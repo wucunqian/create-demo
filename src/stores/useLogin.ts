5a982326bfef2202de12f0ee4b9e8bdf4ed5ef9b
wucunqian <2801531707@qq.com>
2024-07-07 17:11:20 +0800

    feat：新插件pinia
    给项目配置了pinia

---
+++ src/stores/useLogin.ts
@@ -0,0 +1,24 @@
+/**
+ * Author: wucunqian 2801531707@qq.com
+ * Date: 2024-07-07 16:35:01
+ * LastEditors: wucunqian 2801531707@qq.com
+ * LastEditTime: 2024-07-07 16:47:38
+ * FilePath: \create-demo\src\stores\useLogin.ts
+ * Description: pinia 文件一般导出单个，切需要使用 use+var 命名，使用 setup 写法
+ * 
+ * Copyright (c) 2024 by ${wucunqian 2801531707@qq.com}, All Rights Reserved. 
+ */
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+export const useLogin = defineStore('useLogin', () => {
+  interface UseLogin {
+    title: String;
+    msg: String;
+  }
+  const objGather = ref<UseLogin>({
+    title: 'useLogin',
+    msg: "pinia获取",
+    count: 123
+  });
+  return { objGather };
+});
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,16 +2,22 @@
  * @Author: wucunqian 2801531707@qq.com
  * @Date: 2024-07-07 16:07:17
  * @LastEditors: wucunqian 2801531707@qq.com
- * @LastEditTime: 2024-07-07 16:11:51
+ * @LastEditTime: 2024-07-07 16:51:15
  * @FilePath: \create-demo\src\views\login\index.vue
  * @Description: 登录界面
  * 
  * Copyright (c) 2024 by ${wucunqian 2801531707@qq.com}, All Rights Reserved. 
 -->
 <template>
-  <div>login</div>
+  <div>{{ stores.objGather.title }} -- <code>{{ stores.objGather.msg }}</code>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Ref, ref } from 'vue';
+import { useLogin } from '../../stores/useLogin';
+const stores = useLogin();
+
+</script>
 
 <style scoped></style>

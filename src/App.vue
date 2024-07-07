e582328bd4efdfd5833a605b5ea308d66a542bae
wucunqian <2801531707@qq.com>
2024-07-07 15:38:30 +0800

    ci: 集成模板
    1111111

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: wucunqian 2801531707@qq.com
+ * @Date: 2024-07-07 14:14:30
+ * @LastEditors: wucunqian 2801531707@qq.com
+ * @LastEditTime: 2024-07-07 15:25:24
+ * @FilePath: \create-demo\src\App.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by ${wucunqian 2801531707@qq.com}, All Rights Reserved. 
+-->
+<template>
+  <div>阿达</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>

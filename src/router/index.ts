f09c583ea54655ae503fed2e0d5b764fcdce4f7f
wucunqian <2801531707@qq.com>
2024-07-08 20:07:51 +0800

    feat: 配置element-plus

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,7 @@ console.log('History 模式：', createWebHistory());
 const routes: RouteRecordRaw[] = [
   { path: "/", redirect: "/login" },  // 路由重定向
   { path: "/login", name: "login", component: () => import('../views/login/index.vue') },
+  { path: "/table", name: "table", component: () => import('../views/table/index.vue') },
   { path: '/:catchAll(.*)', component: () => import('../views/error/index.vue') },  // 没有路由
 ];
 const router = createRouter({
---
+++ src/views/table/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>table</div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style></style>
